a3454ad42b7d1603044044096ae7c6bf164eb704
atharvaGadhave <140781465+atharvaGadhave@users.noreply.github.com>
2024-07-17 22:37:51 +0530

    Update firebase.js

--- Chatifytest-main/src/firebase.js
+++ Chatifytest-main/src/firebase.js
@@ -4,12 +4,7 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3NDIwm-XUdvvYZXRJKy_ezJRdmNQ9l7M",
-  authDomain: "chatify-final.firebaseapp.com",
-  projectId: "chatify-final",
-  storageBucket: "chatify-final.appspot.com",
-  messagingSenderId: "782574333985",
-  appId: "1:782574333985:web:62c1b7c9a3bc5ac8277360"
+  
 };
 
 // Initialize Firebase
